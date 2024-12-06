64810f8aa96da60f33f759a34601ea7f11545d6c
Chicken-Bones <mehvids@gmail.com>
2024-12-06 17:30:28 +1000

    Initial Commit, Windows Support

---
+++ Patches.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioAchievementPatcher
+{
+	public record Patch(string FunctionName, int Offset, byte[] Target, byte[] Replacement)
+	{
+		internal bool Apply(Span<byte> fnBytes)
+		{
+			fnBytes = fnBytes[Offset..];
+			if (!fnBytes.StartsWith(Target))
+				throw new Exception($"Failed to apply patch ({FunctionName}): Target not found at Offset");
+
+			if (fnBytes.StartsWith(Replacement))
+				return false;
+
+			Replacement.CopyTo(fnBytes);
+			return true;
+		}
+	}
+
+	public static class Patches
+	{
+		public static Patch[] Windows = [
+			new Patch("AchievementGui::updateInGameLongEnoughLabel", Offset: 0x33,
+				Target: [
+					0x48, 0x8B, 0x80, 0x80, 0x01, 0x00, 0x00,  // mov     rax, [rax+180h]
+					0x49, 0x8B, 0x90, 0x58, 0x02, 0x00, 0x00,  // mov     rdx, [r8+258h]  ; tick
+					0x48, 0xD1, 0xE8,                          // shr     rax, 1
+					0x48, 0x3B, 0xD0,                          // cmp     rdx, rax
+				],
+				Replacement: [
+					0x48, 0x8B, 0x80, 0x80, 0x01, 0x00, 0x00,  // mov     rax, [rax+180h]
+					0x49, 0x8B, 0x90, 0x58, 0x02, 0x00, 0x00,  // mov     rdx, [r8+258h]  ; tick
+					0x48, 0x31, 0xC0,                          // xor     rax, rax
+					0x48, 0x3B, 0xD0,                          // cmp     rdx, rax
+				]
+			),
+			new Patch("Player::isOnlineLongEnoughToGetAchievements", Offset: 0,
+				Target: [
+					0x48, 0x8B, 0x41, 0x20,                    // mov     rax, [this+20h]
+					0x48, 0x8B, 0x80, 0x80, 0x01, 0x00, 0x00,  // mov     rax, [rax+180h]
+					0x48, 0xD1, 0xE8,                          // shr     rax, 1
+					0x48, 0x39, 0x81, 0x58, 0x02, 0x00, 0x00,  // cmp     [this+258h], rax
+				],
+				Replacement: [
+					0x48, 0x8B, 0x41, 0x20,                    // mov     rax, [this+20h]
+					0x48, 0x8B, 0x80, 0x80, 0x01, 0x00, 0x00,  // mov     rax, [rax+180h]
+					0x48, 0x31, 0xC0,                          // xor     rax, rax
+					0x48, 0x39, 0x81, 0x58, 0x02, 0x00, 0x00,  // cmp     [this+258h], rax
+				]
+			),
+		];
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using FactorioAchievementPatcher;
+using System.Runtime.InteropServices;
+
+try {
+	if (args.Length <= 0)
+		throw new ArgumentException($"Usage: <path to factorio.exe or map.zip");
+
+	var modulePath = args[0];
+	if (!File.Exists(modulePath))
+		throw new ArgumentException($"File not found: {modulePath}");
+
+	if (Path.GetExtension(modulePath) is not ".exe")
+		throw new ArgumentException($"{Path.GetFileName(modulePath)} does not end in .exe");
+
+	var moduleBytes = File.ReadAllBytes(modulePath);
+
+	bool applied = false;
+	if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+		using var windowsSymHelper = new WindowsSymbolHelper(modulePath);
+
+		foreach (var patch in Patches.Windows) {
+
+			var fnOffset = windowsSymHelper.GetFunctionOffset(patch.FunctionName);
+			var fnBytes = moduleBytes.AsSpan(start: (int)fnOffset);
+			if (patch.Apply(fnBytes)) {
+				Console.WriteLine($"Patched {patch.FunctionName}");
+				applied = true;
+			}
+			else {
+				Console.WriteLine($"Already patched {patch.FunctionName}");
+			}
+		}
+	}
+	else {
+		throw new NotSupportedException($"Unsupported platform: {RuntimeInformation.OSDescription}");
+	}
+
+	if (applied)
+		File.WriteAllBytes(modulePath, moduleBytes);
+
+	Console.WriteLine("Done");
+}
+catch (ArgumentException ex) {
+	Console.Error.WriteLine(ex.Message);
+}
+catch (Exception ex) {
+	Console.Error.WriteLine(ex);
+}
